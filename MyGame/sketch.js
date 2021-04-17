fabb4a7ecf11c5f4c4c27bdd72624d8c65cb8ca4
OliverG2020 <76639822+OliverG2020@users.noreply.github.com>
2021-04-16 20:30:23 -0700

    Add files via upload

---
+++ MyGame/sketch.js
@@ -0,0 +1,19 @@
+var gameState = "start"
+var PC,bg1,bg2,bg3,bg4,bg4,bg5,arrow,dialogbox
+
+
+function preload() {
+  
+  
+}
+
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
